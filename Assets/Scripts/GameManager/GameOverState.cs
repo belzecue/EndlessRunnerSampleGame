1d29751ba347eedbae23349ac0b541deff6ff7c1
Guillaume <guillaume@unity3d.com>
2017-07-03 15:57:49 +0100

    Original commit from latest asset store version

---
+++ Assets/Scripts/GameManager/GameOverState.cs
@@ -0,0 +1,283 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_ADS
+using UnityEngine.Advertisements;
+#endif
+#if UNITY_ANALYTICS
+using UnityEngine.Analytics;
+using UnityEngine.Analytics.Experimental;
+#endif
+using System.Collections.Generic;
+ 
+/// <summary>
+/// state pushed on top of the GameManager when the player dies.
+/// </summary>
+public class GameOverState : AState
+{
+    public TrackManager trackManager;
+    public Canvas canvas;
+    public MissionUI missionPopup;
+
+	public AudioClip gameOverTheme;
+
+    public Button adsButton;
+    public string adsPlacementId = "rewardedVideo";
+#if UNITY_ANALYTICS
+    public AdvertisingNetwork adsNetwork = AdvertisingNetwork.UnityAds;
+#endif
+    public bool adsRewarded = true;
+
+	public Leaderboard miniLeaderboard;
+	public Leaderboard fullLeaderboard;
+
+    public GameObject addButton;
+
+	protected bool m_CoinCredited = false;
+
+    public override void Enter(AState from)
+    {
+        canvas.gameObject.SetActive(true);
+
+		miniLeaderboard.playerEntry.inputName.text = PlayerData.instance.previousName;
+		
+		miniLeaderboard.playerEntry.score.text = trackManager.score.ToString();
+		miniLeaderboard.Populate();
+
+        if (PlayerData.instance.AnyMissionComplete())
+            missionPopup.Open();
+        else
+            missionPopup.gameObject.SetActive(false);
+
+		adsButton.gameObject.SetActive(false);
+
+		m_CoinCredited = false;
+
+		if (trackManager.isRerun)
+			CreditCoins();
+
+		if (MusicPlayer.instance.GetStem(0) != gameOverTheme)
+		{
+            MusicPlayer.instance.SetStem(0, gameOverTheme);
+			StartCoroutine(MusicPlayer.instance.RestartAllStems());
+        }
+
+#if UNITY_ADS
+		if (!trackManager.isRerun && Advertisement.IsReady(adsPlacementId))
+		{
+			adsButton.GetComponentInChildren<Text>().text = "AD for 1-up";
+			addButton.SetActive(true);
+#if UNITY_ANALYTICS
+            AnalyticsEvent.AdOffer(adsRewarded, adsNetwork, adsPlacementId, new Dictionary<string, object>
+            {
+                { "level_index", PlayerData.instance.rank },
+                { "distance", TrackManager.instance == null ? 0 : TrackManager.instance.worldDistance },
+            });
+#endif
+        }
+#endif
+    }
+
+	public override void Exit(AState to)
+    {
+        canvas.gameObject.SetActive(false);
+
+        if (!trackManager.isRerun)
+        {
+            FinishRun();
+        }
+    }
+
+    public override string GetName()
+    {
+        return "GameOver";
+    }
+
+    public override void Tick()
+    {
+        
+    }
+
+	public void OpenLeaderboard()
+	{
+		fullLeaderboard.forcePlayerDisplay = false;
+		fullLeaderboard.displayPlayer = true;
+		fullLeaderboard.playerEntry.playerName.text = miniLeaderboard.playerEntry.inputName.text;
+		fullLeaderboard.playerEntry.score.text = trackManager.score.ToString();
+
+		fullLeaderboard.Open();
+    }
+
+	public void GoToStore()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("shop", UnityEngine.SceneManagement.LoadSceneMode.Additive);
+    }
+
+
+    public void GoToLoadout()
+    {
+        trackManager.isRerun = false;
+		manager.SwitchState("Loadout");
+    }
+
+    public void RunAgain()
+    {
+        trackManager.isRerun = false; 
+		manager.SwitchState("Game");
+    }
+
+    //return to currently lost game (i.e. when watched an ad for second wind)
+    public void BackToRun()
+    {
+        trackManager.isRerun = true;
+        trackManager.characterController.currentLife += 1;
+        manager.SwitchState("Game");
+    }
+
+	protected void CreditCoins()
+	{
+		if (m_CoinCredited)
+			return;
+
+		// -- give coins gathered
+		PlayerData.instance.coins += trackManager.characterController.coins;
+		PlayerData.instance.premium += trackManager.characterController.premium;
+
+		PlayerData.instance.Save();
+
+#if UNITY_ANALYTICS // Using Analytics Standard Events v0.3.0
+        var transactionId = System.Guid.NewGuid().ToString();
+        var transactionContext = "gameplay";
+        var level = PlayerData.instance.rank.ToString();
+        var itemType = "consumable";
+        
+        if (trackManager.characterController.coins > 0)
+        {
+            AnalyticsEvent.ItemAcquired(
+                AcquisitionType.Soft, // Currency type
+                transactionContext,
+                trackManager.characterController.coins,
+                "fishbone",
+                PlayerData.instance.coins,
+                itemType,
+                level,
+                transactionId
+            );
+        }
+
+        if (trackManager.characterController.premium > 0)
+        {
+            AnalyticsEvent.ItemAcquired(
+                AcquisitionType.Premium, // Currency type
+                transactionContext,
+                trackManager.characterController.premium,
+                "anchovies",
+                PlayerData.instance.premium,
+                itemType,
+                level,
+                transactionId
+            );
+        }
+#endif 
+
+        m_CoinCredited = true;
+	}
+
+	protected void FinishRun()
+    {
+
+		CreditCoins();
+
+		if(miniLeaderboard.playerEntry.inputName.text == "")
+		{
+			miniLeaderboard.playerEntry.inputName.text = "Trash Cat";
+		}
+		else
+		{
+			PlayerData.instance.previousName = miniLeaderboard.playerEntry.inputName.text;
+		}
+
+        PlayerData.instance.InsertScore(trackManager.score, miniLeaderboard.playerEntry.inputName.text );
+
+        CharacterCollider.DeathEvent de = trackManager.characterController.characterCollider.deathData;
+        //register data to analytics
+#if UNITY_ANALYTICS
+        AnalyticsEvent.GameOver(null, new Dictionary<string, object> {
+            { "coins", de.coins },
+            { "premium", de.premium },
+            { "score", de.score },
+            { "distance", de.worldDistance },
+            { "obstacle",  de.obstacleType },
+            { "theme", de.themeUsed },
+            { "character", de.character },
+        });
+#endif
+
+        PlayerData.instance.Save();
+
+        trackManager.End();
+    }
+
+    //----------------
+
+
+    public void ShowRewardedAd()
+    {
+#if UNITY_ADS
+        if (Advertisement.IsReady(adsPlacementId))
+        {
+#if UNITY_ANALYTICS
+            AnalyticsEvent.AdStart(adsRewarded, adsNetwork, adsPlacementId, new Dictionary<string, object>
+            {
+                { "level_index", PlayerData.instance.rank },
+                { "distance", TrackManager.instance == null ? 0 : TrackManager.instance.worldDistance },
+            });
+#endif
+
+            adsButton.GetComponentInChildren<Text>().text = "Loading...";
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show(adsPlacementId, options);
+        }
+        else
+        {
+#if UNITY_ANALYTICS
+            AnalyticsEvent.AdSkip(adsRewarded, adsNetwork, adsPlacementId, new Dictionary<string, object> {
+                { "error", Advertisement.GetPlacementState(adsPlacementId).ToString() }
+            });
+#endif
+        }
+#else
+		BackToRun();
+#endif
+    }
+
+	//=== AD
+#if UNITY_ADS
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+#if UNITY_ANALYTICS
+                AnalyticsEvent.AdComplete(adsRewarded, adsNetwork, adsPlacementId);
+#endif
+                BackToRun();
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+#if UNITY_ANALYTICS
+                AnalyticsEvent.AdSkip(adsRewarded, adsNetwork, adsPlacementId);
+#endif
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown.");
+#if UNITY_ANALYTICS
+                AnalyticsEvent.AdSkip(adsRewarded, adsNetwork, adsPlacementId, new Dictionary<string, object> {
+                    { "error", "failed" }
+                });
+#endif
+                break;
+        }
+    }
+#endif
+}
